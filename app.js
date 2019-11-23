6a58831e7ac007ce630e383b9a5c4f014675287c
W Nowak <bulion@zoho.com>
2019-11-23 22:37:42 +0100

    init

---
+++ app.js
@@ -0,0 +1,86 @@
+let tempSpan = document.getElementById("tempval");
+let humSpan = document.getElementById("humval");
+let altTempSpan = document.getElementById("alttempval");
+
+function setTemp(temp) {
+    tempSpan.innerText = temp + "\u00B0C";
+}
+
+function setHum(hum) {
+    humSpan.innerText = hum + "%";
+}
+
+function setAltTemp(temp) {
+    altTempSpan.innerText = temp + "\u00B0C";
+}
+
+function getTemp() {
+    return parseFloat(tempSpan.innerText.replace("\u00B0C", ""));
+}
+
+function getHum() {
+    return parseFloat(humSpan.innerText.replace("%", ""));
+}
+
+function getAltTemp() {
+    return parseFloat(altTempSpan.innerText.replace("\u00B0C", ""));
+}
+var executed = false
+
+function changeNumber(val, getVal, setVal) {
+
+    if (val != getVal()) {
+        var range = val - getVal();
+        var increment = range / 100;
+        if (Math.abs(increment) < 0.01) increment = val > getVal() ? 0.01 : -0.01;
+        var step = Math.abs(Math.floor(100 / (1 + Math.floor(range))))
+        var timer = setInterval(() => {
+            setVal((getVal() + increment).toPrecision(4));
+            if (Math.abs(val - getVal()) < Math.abs(range / 10)) {
+                clearInterval(timer);
+                setVal(val);
+            }
+        }, step);
+    }
+
+}
+
+setInterval(() => {
+    fetch("http://178.183.121.110/").then((resp) => {
+        resp.json().then((resp => {
+            changeNumber(parseFloat(resp["temp"]), getTemp, setTemp);
+            changeNumber(parseFloat(resp["hum"]), getHum, setHum);
+            changeNumber(parseFloat(resp["temp_2"]), getAltTemp, setAltTemp);
+        }));
+    });
+}, 1000);
+
+let bg = document.getElementsByClassName("bg-wideo");
+
+window.onload = function () {
+    fetch("http://178.183.121.110/").then((resp) => {
+        resp.json().then((resp => {
+            changeNumber(parseFloat(resp["temp"]), getTemp, setTemp);
+            changeNumber(parseFloat(resp["hum"]), getHum, setHum);
+            changeNumber(parseFloat(resp["temp_2"]), getAltTemp, setAltTemp);
+        }));
+    });
+    document.getElementById("titlescreen").style.opacity = 1;
+    if (Math.random() > 0.5)
+        bg[0].style.opacity = 1;
+    else bg[1].style.opacity = 1;
+}
+
+setInterval(() => {
+    if (Math.random() > 0.5) {
+        if (bg[1].style.opacity == 0 && bg[0].style.opacity == 1) {
+            bg[1].style.opacity = 1;
+            setTimeout(bg[0].style.opacity = 0, 1000);
+        }
+    } else {
+        if (bg[0].style.opacity == 0 && bg[1].style.opacity == 1) {
+            bg[0].style.opacity = 1;
+            setTimeout(bg[1].style.opacity = 0, 1000);
+        }
+    }
+}, 10000);
